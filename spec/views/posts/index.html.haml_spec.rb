45e97381be9b6ff5540231ae16862a8c1198b95b
tmszrbn <urbi90@gmail.com>
2018-05-03 13:04:12 +0200

    fixes

--- spec/views/posts/index.html.haml_spec.rb
+++ spec/views/posts/index.html.haml_spec.rb
@@ -4,19 +4,25 @@ RSpec.describe "posts/index.html.haml", type: :view do
   let(:user1) { create(:user) }
   let(:user2) { create(:user) }
   let(:post) { create(:post, user_id: user1.id) }
-  let(:comment1) { create(:comment, user_id: user1.id, post_id: post.id)}
-  let(:comment2) { create(:comment, user_id: user2.id, post_id: post.id)}
-
+  let!(:comment1) { create(:comment, user_id: user1.id, commentable_id: post.id)}
+  let!(:comment2) { create(:comment, user_id: user2.id, commentable_id: post.id)}
+  
   before do
-    assign(:posts, Post.all)
-    # assigns(:likes) = Post.all
+    allow(controller).to receive(:current_user).and_return(user1)
+    render partial: 'posts/post.html.haml', locals: { post: post }
   end
   it 'displays post\'s content' do
-    render
-    expect(rendered).to render_template(partial: 'post')
+    expect(rendered).to include(post.content)
   end
-  it 'displays post\'s author'
-  it 'displays post\'s comments'
-  it 'displays post\'s likes'
+  it 'displays post\'s author' do
+    expect(rendered).to include(post.user.name)
+  end
+  it 'displays post\'s likes count' do
+    expect(rendered).to include("#{post.likes.count}")
+  end
+  it 'displays post\'s comments' do
+    expect(rendered).to include(comment1.content)
+    expect(rendered).to include(comment2.content)
+  end
 
 end
