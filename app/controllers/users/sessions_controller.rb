b32e6e6dbe86d8620bed52a05fe9437c8f074f34
tmszrbn <urbi90@gmail.com>
2018-05-06 11:50:34 +0200

    signing in redirection loop fixed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,7 +18,12 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+
+  def after_sign_in_path_for(resource)
+    users_index_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
