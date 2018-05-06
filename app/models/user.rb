ff388b2e87c7e07ddd4c49ea4671c82fdf4da507
tmszrbn <urbi90@gmail.com>
2018-05-06 07:20:47 +0200

    fix - send friend request button

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i[facebook]
   
   has_many :posts
   has_many :comments
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   has_many :sent_friendship_requests, class_name: "FriendshipRequest", foreign_key: 'sender_id'
 
   has_one  :image, as: :imageable
+
 
   def send_friendship_request receiver
     unless self.friendship_request_from?(receiver) || self.friendship_request_to?(receiver) || self.friends_with?(receiver) || self.equal?(receiver)
@@ -53,7 +54,7 @@ class User < ApplicationRecord
     !self.friends_with?(user) && self.friendship_request_from?(user)
   end
   def can_send_friendship_request_to? user
-    !self.friends_with?(user) && !self.friendship_request_from?(user) && !self.friendship_request_to?(user) && self == user
+    !self.friends_with?(user) && !self.friendship_request_from?(user) && !self.friendship_request_to?(user) && self != user
   end
 
   def create_post title, content
@@ -70,4 +71,23 @@ class User < ApplicationRecord
   def add_comment post, comment_body
     post.comments.create user_id: self.id, content: comment_body
   end
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
+    # user.image = auth.info.image # assuming the user model has an image
+    # If you are using confirmable and the provider(s) you use validate emails, 
+    # uncomment the line below to skip the confirmation emails.
+    # user.skip_confirmation!
+  end
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+end
 end
