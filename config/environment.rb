b575dc6a1f9f94d0f86af8908d7ceda1171c2bc5
tmszrbn <urbi90@gmail.com>
2018-05-06 12:05:13 +0200

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'whispering-retreat-66818.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+
+  ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'yourdomain.com',
